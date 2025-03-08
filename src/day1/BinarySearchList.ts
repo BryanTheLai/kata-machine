c3f637ecc8ed49af80ff71e9e3eec69edc482492
NotABot <135363764+BryanTheLai@users.noreply.github.com>
2025-03-08 17:51:34 +0800

    Binary and jumping search
    Binary search is just split into half and search. O(Log n).
    
    TwoCrystalBalls problem uses jumping, specirfically sqrt of length of array.
    
    jump first, then when something happens, go back by 1 and do linear search from there. O(sqrt(n))

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let l = 0;
+    let r = haystack.length;
 
+    do {
+        const m = Math.floor(l + (r - l) / 2);
+
+        if (needle === haystack[m]) {
+            return true;
+        }
+
+        if (needle > haystack[m]) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    } while (l < r);
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,15 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmp = Math.floor(Math.sqrt(breaks.length));
+    let startIdx = 0;
 
+    for (; startIdx < breaks.length; startIdx += jmp) {
+        if (breaks[startIdx]) break;
+    }
+
+    startIdx -= jmp;
+
+    for (let x = startIdx; x < startIdx + jmp; x++) {
+        if (breaks[x]) return x;
+    }
+    return -1;
 }
